e2afdf442c27210259b781c19328a6556cc8fd55
Shogo Hashimoto <50n50n.xarc@gmail.com>
2023-02-26 00:10:26 +0900

    add ding feature

---
+++ Scrumdinger/Models/AVPlayer+Ding.swift
@@ -0,0 +1,15 @@
+//
+//  AVPlayer+Ding.swift
+//  Scrumdinger
+//
+//  Created by sy-hash on 2023/02/25.
+//
+
+import AVFoundation
+
+extension AVPlayer {
+    static let sharedDingPlayer: AVPlayer = {
+        guard let url = Bundle.main.url(forResource: "ding", withExtension: "wav") else { fatalError("Failed to find sound file.") }
+        return AVPlayer(url: url)
+    }()
+}
