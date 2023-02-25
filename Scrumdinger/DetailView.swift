8fb3e3ddc15bd171ea59b5a9f44022e0de387d61
Shogo Hashimoto <50n50n.xarc@gmail.com>
2023-02-25 15:18:20 +0900

    Add visual components to the detail view

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,43 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by sy-hash on 2023/02/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }.accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+            }
+        }
+    }
+}
+
+struct DetailView_Provider: PreviewProvider {
+    static var previews: some View {
+        DetailView(scrum: DailyScrum.sampleData[0])
+    }
+}
--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -14,7 +14,7 @@ struct ScrumsView: View {
     var body: some View {
         List {
             ForEach(scrums) { scrum in
-                NavigationLink(destination: Text(scrum.title)) {
+                NavigationLink(destination: DetailView(scrum: scrum)) {
                     CardView(scrum: scrum)
                         .listRowBackground(scrum.theme.mainColor)
                 }.listRowBackground(scrum.theme.mainColor)
